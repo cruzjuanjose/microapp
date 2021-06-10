d2f19bab6d22fc347f38532b8b6974138352d69f
Erick Cortes <devone@thinkotb.io>
2021-06-10 12:17:34 -0600

    fix

--- App.jsx
+++ App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+
+var Iframe = React.createClass({     
+  render: function() {
+    return(         
+      <div>          
+        <iframe src={this.props.src} height={this.props.height} width={this.props.width}/>         
+      </div>
+    )
+  }
+});
+
 class App extends React.Component {
   render() {
     return (
-      <div>simon, helloworld!!!</div>
+      <main>  
+
+     <h1>Iframe test </h1>
+     <iframe src="https://calculator-react-ugbq6sejbq-uc.a.run.app/" title="test">  </iframe>
+  </main>
     );
   }
 }
